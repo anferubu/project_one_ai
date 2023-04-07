ef5180b08e903c658de08e785f2cc8bb7b48493b
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2023-04-07 17:42:55 -0500

    Move principal functions to gui

--- app/main.py
+++ app/main.py
@@ -1,19 +1,7 @@
-from bfs import BFS
 from gui import Board
-from maze import Maze
-
 
 if __name__ == '__main__':
-    # Open the file with a matrix
-    maze = Maze('./data/matrix2.txt')
-
-    # Define the matrix
-    matrix = maze.matrix
-
-    # Define the path
-    bfs = BFS(maze)
-    path = bfs.solve_zigzag()  # .solve() for normal BFS
 
     # Run the app
-    app = Board(matrix, path)
+    app = Board('./data/matrix.txt')
     app.window.mainloop()
